149043e04e9fb206973eca0786abde5e1777eb56
Rafael Gomez <rafitagd90@gmail.com>
2024-03-16 08:06:13 +0000

    Add UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>
-    <h2>Hello {value}</h2>
+    <h2>Computer Vision</h2>
     <p>Insert URL, or type prompt</p>
     <input type="text" />
     <button>Analyze</button>
