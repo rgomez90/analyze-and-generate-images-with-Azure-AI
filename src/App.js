cff9ff470381ce316d1c6b1f432d7ed143f39a4f
Rafael Gomez <rafitagd90@gmail.com>
2024-03-16 08:05:00 +0000

    add UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h2>Hello {value}</h2>
+    <p>Insert URL, or type prompt</p>
+    <input type="text" />
+    <button>Analyze</button>
+    <button>Generate</button>
+    </div>;
 }
 
 export default App;
